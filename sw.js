e5d5677ce73835c85d6980da11850d60b3fb09b3
goldypasulu19 <goldypasulu19@gmail.com>
2023-03-31 08:50:14 +0700

    coba3

--- sw.js
+++ sw.js
@@ -1,32 +1,48 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  '/manifest.json',
-  '/assets/images/icon-192x192.png',
-  '/assets/images/icon-512x512.png'
-];
-
 self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  self.skipWaiting(); //PENTING bila ada versi baru!!
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches.open('static')
       .then(function(cache) {
-        return cache.addAll(urlsToCache);
+        console.log('[Service Worker] Precaching App Shell');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/app.js',
+          '/blog.html',
+          '/about.html',
+          '/style.css',
+          '/contact.html'
+        ]);
+      })
+  )
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
       })
   );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request);
+    caches.open(CACHE_DYNAMIC_NAME)
+      .then(function(cache) {
+        return fetch(event.request)
+          .then(function(res) {
+            cache.put(event.request, res.clone());
+            return res;
+          });
       })
   );
 });
