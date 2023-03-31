1f85ae9f42276b7166844acf45eefa2d604d4041
goldypasulu19 <goldypasulu19@gmail.com>
2023-03-31 08:24:18 +0700

    coba2

--- sw.js
+++ sw.js
@@ -1,54 +1,32 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/manifest.json',
+  '/assets/images/icon-192x192.png',
+  '/assets/images/icon-512x512.png'
+];
+
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('first-app')
-        .then(function(cache) {
-          cache.addAll([
-            '/',
-            '/index.html',
-            '/styles.css',
-            '/app.js'
-          ])
-        })
-    );
-    return self.clients.claim();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
-  
-self.addEventListener('activate', function(event) {
-    console.log('[Service Worker] Activating Service Worker ....', event);
-    event.waitUntil(
-      caches.keys()
-        .then(function(keyList) {
-          return Promise.all(keyList.map(function(key) {
-            if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-              console.log('[Service Worker] Removing old cache.', key);
-              return caches.delete(key);
-            }
-          }));
-        })
-    );
-    return self.clients.claim();
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          if (response) {
-            return response;
-          } else {
-            return fetch(event.request)
-              .then(function(res) {
-                return caches.open(CACHE_DYNAMIC_NAME)
-                  .then(function(cache) {
-                    cache.put(event.request.url, res.clone());
-                    return res;
-                  })
-              })
-              .catch(function(err) {
-  
-              });
-          }
-        })
-    );
-  });
-  
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request);
+      })
+  );
+});
