c09383bfd5c51f9961b506867204ae657eb6bbf6
goldypasulu19 <goldypasulu19@gmail.com>
2023-03-31 09:17:57 +0700

    coba5

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var title = document.querySelector('.title');
-var courseFeatureElements = document.querySelectorAll('.course-feature');
-var button = document.querySelector('button');
-
 navigator.serviceWorker.register('/sw.js');
+
 
 var deferredPrompt;
 window.addEventListener('beforeinstallprompt', function(event) {
